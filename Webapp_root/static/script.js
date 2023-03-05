7b58014d16abea21c52517974ba3d59d9383771c
zachRadack <104032269+zachRadack@users.noreply.github.com>
2023-03-05 15:20:16 -0700

    refactored the js and overhauled the miniwidgets
    I refactored the javascript to be way more understandable,
    
    where to add mini widgets and how they function has been clearly layed out to the best of my abilities.
    
    
    The area to add mini widget code has been added to the bottom of the javascript!!
    
    
    Now mini widgets get properly sorted and also can be transfered between widgets. I need to figure out why when dragging to another
    main widget they go under the widget....

--- Webapp_root/static/script.js
+++ Webapp_root/static/script.js
@@ -2,145 +2,158 @@
 // untill all the html is finished loading,
 // critical for all jquerey or else it make get ~funky~
 $(document).ready(function() {
-
-    // Makes main widget draggable
-    $(".main_widget").draggable({
+	// Makes main widget and mini widget draggable
+	// this only affects the ones who load in at widget factory
+    $(".main_widget, .mini-widget").draggable({
         helper: "clone",
         revert: "invalid",
         start: function(e, ui) {
             ui.helper.addClass("dragging"); // add class to differentiate from original element
-          },
+        },
         stop: function(e, ui) {
-            ui.helper.removeClass("dragging"); // remove class on stop
-          }
+            ui.helper.removeClass("dragging");// remove class on stop
+        }
     });
-
-    // Makes mini widget draggable
-    $(".mini-widget").draggable({
-        helper: "clone",
-        revert: "invalid",
-        start: function(e, ui) {
-            ui.helper.addClass("dragging"); // add class to differentiate from original element
-          },
-        stop: function(e, ui) {
-            ui.helper.removeClass("dragging"); // remove class on stop
-          }
-    });
-
-    // Make canvas accept droppables
+	
+	// deals with the canvas, the area where widgets are placed around
+    // this also handles main widget
     $(".canvas").droppable({
-        drop: function drop(ev, ui) {
-            // this makes it so only main_widget is accepted by canvas, the other methods seemed to get 
-            // really werid
+        drop: function(ev, ui) {
             if (ui.draggable.hasClass("main_widget")) {
                 var canvas = $('.canvas');
                 if (ui.draggable.attr('data-new') === 'true') {
                     // if it's a new widget, then it means we do not want to rip it out of the factory
                     // this part makes it so it just clones it instead
-                    box = widget_clone(ui,canvas);
-                    droppable_continer_cloned(box.children('.widget-container'));
-                    canvas_css_Setup_canvas(ui,box,canvas,"true")
-                }else{
+                var box = widget_clone(ui, canvas);
+                droppable_continer_cloned(box.children('.widget-container'));
+                canvas_css_Setup_canvas(ui, box, canvas, 'true');
+            }else{
                     // if its not a new widget, it gets not cloning, because I am mean like that
-                    var box = ui.draggable;
-                    canvas.append(box);
-                    canvas_css_Setup_canvas(ui,box,canvas,"false")
+                    canvas.append(ui.draggable);
+                    canvas_css_Setup_canvas(ui, box, canvas, 'false');
                 }
-          }}
-      });
-
-      
-    
-})
-
-// This makes the widget_container so it can accept droppable mini widgets
-function droppable_continer_cloned(container){
-    container.droppable({
-        drop: function drop(ev, ui) {
-            // this makes it so only main_widget is accepted by widgetContainer, the other methods seemed to get 
-            // really werid, we could also expand this by changing the mini-widget bit to change what we want it to be
-            if (ui.draggable.hasClass("mini-widget")) {
-                // grabs current widget
-                var widgetContainer = $(this);
-                if (ui.draggable.attr('data-new') === 'true') {
-                    // if it's a new widget, then it means we do not want to rip it out of the factory
-                    // this part makes it so it just clones it instead
-                    console.log("test1");
-                    box = widget_clone(ui,widgetContainer);
-                    console.log("test2");
-                    console.log($(this).parent().width());
-
-                    box.css({
-                        
-                        width: $(this).parent().width(),
-
-                    });
-                    if(ui.draggable.hasClass("textMiniWidget")){
-                        text_widget(ui,box)
-                    }
-                    
-                    //canvas_css_Setup_canvas_widget(ui,box,canvas,"true")
-                }else{
-                    // if its not a new widget, it gets not cloning, because I am mean like that
-                    var box = ui.draggable;
-                    widgetContainer.append(box);
-                    //canvas_css_Setup_canvas_widget(ui,box,canvas,"false")
-                }
-        }
-        }
-      // end of ready function
+                
+            }
+          }
     });
-};
+	});
 
-
-function widget_clone(ui,canvas) {
-    //Clone widget, this was made to be as universal as possible
-    var box = ui.draggable.clone();
-    canvas.append(box);
-    // Make the cloned element draggable, otherwise it would set and never move
-    box.draggable({
-        containment: "parent", // keep the element within the canvas
-            start: function(e, ui) {
-            ui.helper.addClass("dragging"); // add class to differentiate from original element
-        },
-        stop: function(e, ui) {
-            ui.helper.removeClass("dragging"); // remove class on stop
-        }
-    });
-    box.attr('data-new', false);
-    return box
-}
-
-
-
-
-
-
-function canvas_css_Setup_canvas(ui,box,canvas,new_widget){
-    //sets the position, if new_widget is true, then it will account for 
-    // the appending of the thing to whatever canvas its being appended too.
-    if(new_widget==="true"){
-        // Set the position of the box based on the mouse position
-        box.css({
+    // when a main widget is added to canvas
+    // its position become absolute and coordinates account for the 
+    // canvas, so it should drop near where the user dropped it
+    function canvas_css_Setup_canvas(ui, box, canvas, new_widget) {
+        var position = {
             'position': 'absolute',
             'left': ui.position.left - canvas.offset().left + 'px',
-            'top': ui.position.top - canvas.offset().top+ 'px'
+            'top': ui.position.top - canvas.offset().top + 'px'
+        };
+        if (!new_widget) {
+            position = {
+                'position': 'absolute',
+                'left': ui.position.left  + 'px',
+                'top': ui.position.top+ 'px'
+            };
+        }
+        box.css(position);
+    }
+	
+    // This makes the widget_container so it can accept droppable mini widgets
+    function droppable_continer_cloned(container) {
+        container.droppable({
+            drop: function(ev, ui) {
+                if (ui.draggable.hasClass("mini-widget")) {
+                    var widgetContainer = $(this);
+                    if (ui.draggable.attr('data-new') === 'true') {
+                        var box = widget_clone(ui, widgetContainer);
+                        }else{
+                            widgetContainer.append(box);
+                        }
+                }
+            },
         });
-    }else{
-        box.css({
-            'position': 'absolute',
-            'left': ui.position.left  + 'px',
-            'top': ui.position.top+ 'px'
+
+        // adds sortable to widget container so it can sort miniwidgets
+        // This is what allows users to move mini widgets around
+        container.sortable({
+            connectWith: ".widget-container"
         });
     }
-}
 
-function text_widget(ui,box){
 
-    var input = document.createElement("textarea");
-    input.value = "Box";
-    input.onblur = function() {
-      input.value = input.value || "Box";
-    };
-    box.append(input);
-}
+    // clones widgets based on specific class stuff.
+    // not s
+    function widget_clone(ui, canvas) {
+        var box = ui.draggable.clone();
+        canvas.append(box);
+        if (box.hasClass("main_widget")) {
+            main_widgets_maker(box);
+        } else if (box.hasClass("mini-widget")){
+            mini_widgets_maker(ui, canvas, box)
+        }else{
+            box.draggable({
+                start: function(e, ui) {
+                    ui.helper.addClass("dragging");
+                },
+                stop: function(e, ui) {
+                    ui.helper.removeClass("dragging");
+                }
+            });
+        }
+        box.attr('data-new', false);
+        return box;
+    }
+
+    function main_widgets_maker(box) {
+        // this creates a main widget which is draggable
+        // What ever you add to this will be applied to the main
+        // widgets in the canvas
+        box.children('.widget-container').css('width', box.width());
+        box.draggable({
+            containment: "parent",
+            start: function(e, ui) {
+                ui.helper.addClass("dragging");
+            },
+            stop: function(e, ui) {
+                ui.helper.removeClass("dragging");
+            }
+        });
+    }
+
+    function mini_widgets_maker(ui, canvas, box){
+        // this sets the css so that it matches
+        // parent width and position to 
+        // realitve so it won't be draggable to whereever
+        box.css({
+            position: 'relative',
+            width:  box.parent().width(),
+        });
+        /////////////////////////////////////////
+        // To add mini widgets, add a hasClass
+        // check here that checks the mini widgets class
+        // it will then add whatever to the mini widget to make it work
+        // to add style, check the css file or ask zach
+        ////////////////////////////////////////
+        if (ui.draggable.hasClass("textMiniWidget")) {
+            text_widget(ui, box);
+        }
+    }
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+        // THIS IS THE MINI WIDGET AREA, MINI WIDGET VARIANTS CAN BE
+        // ADDED BELOW HERE
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+        ////////////////////////////////////////
+        // this adds text area to the mini widget
+        function text_widget(ui,box){
+            var input = document.createElement("textarea");
+            input.value = "Box";
+            input.onblur = function() {
+            input.value = input.value || "Box";
+            };
+            box.append(input);
+        }
+
+
+
