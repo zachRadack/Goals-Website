75b9eedcfa168b4579fafe173070d7dd65fd29eb
unknown <radack.zach@gmail.com>
2023-03-12 19:09:06 -0700

    added image capabilities
    I added the start of login button along with the ability to call in images now

--- Webapp_root/app.py
+++ Webapp_root/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/static')
 
-@app.route("/")
+@app.route("/",methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
