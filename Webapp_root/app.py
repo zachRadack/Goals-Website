bab2f4c74ba3b0c9061be4f62e92713c13393b18
Shrewdent <87387506+Shrewdent@users.noreply.github.com>
2023-03-13 11:26:55 -0700

    connected htmls

--- Webapp_root/app.py
+++ Webapp_root/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__,static_url_path='/static')
 
 @app.route("/",methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
